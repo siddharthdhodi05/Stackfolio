b1e946c1d96556785db26f880856df306ca5e1dd
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-05-28 17:38:39 +0530

    Step 1 completed:     - Did an npm init to generate a package.json file.     - Install express.js     - Did backend setup

---
+++ server/main.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 5001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running... ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
