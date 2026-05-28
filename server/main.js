dcee3ee1c7879438e8430f19864ff72683d3ea6b
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-05-28 18:24:43 +0530

    Step 4 completed:     - Added MONGO_URI in the .env (MONGO_URI=mongodb://localhost:27017/stackfolio)     - Installed mongoose     - Added db.config.js config file and used it in the main.js file     - Tested the connection by running the server

--- server/main.js
+++ server/main.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import connectDB from "./config/db.config.js";
+
 dotenv.config();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running... ");
