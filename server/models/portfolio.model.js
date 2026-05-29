4ef28e0bd7d79dee931f08504e5a789db9c24f45
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-05-29 17:43:29 +0530

    Step 6 completed:     - Added user and portfolio mongoose models.

---
+++ server/models/portfolio.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  projectName: {
+    type: String,
+    required: [true, "Project's name is required"],
+    trim: true,
+  },
+  projectImage: {
+    type: String,
+    required: [true, "Project image is required"],
+  },
+  projectDescription: {
+    type: String,
+    required: [true, "Project description is required"],
+  },
+  projectStack: [
+    {
+      type: String,
+      required: [true, "Please mention the stack used"],
+    },
+  ],
+  github: {
+    type: String,
+    match: /^https?:\/\/.+/,
+  },
+  liveLink: {
+    type: String,
+  },
+});
+
+const portfolioSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    skills: [{ type: String }],
+    socials: {
+      github: { type: String, match: /^https?:\/\/.+/ },
+      linkedIn: { type: String, match: /^https?:\/\/.+/ },
+      twitter: { type: String, match: /^https?:\/\/.+/ },
+    },
+    projects: [projectSchema],
+  },
+  {
+    timestamps: true,
+    collection: "portfolios",
+  },
+);
---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Collection } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User's full name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "User's email is required"],
+      unique: [true, "User's email must be unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "User's password is required"],
+    },
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  },
+);
+
+const UserModel = mongoose.model("UserModel", userSchema);
+
+export default UserModel;
