619ab21ba485532ea951648d8689c513319bddbb
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-05-30 17:30:32 +0530

    Step 16 completed:     - Implemented logout controller

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -52,7 +52,11 @@ const registerUser = async (req, res) => {
  * @access	Private
  */
 const logoutUser = async (req, res) => {
-  res.send("Logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User logged out" });
 };
 
 /**
