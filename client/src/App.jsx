65e037dcbf3c4ed54d806b50421f34c126534aca
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-06-09 23:53:24 +0530

    Step 29 completed:     - Installed redux toolkit and react redux (npm i @reduxjs/toolkit react-redux)     - Added store.js, slices/apiSlice.js, constants.js     - App.jsx was setup with the Redux provider and the store     - Added slices folder to jsconfig.json and package.json     - Added script to run server and client concurrently in server/package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "@components/Layout";
 import Home from "@screens/HomeScreen";
 import ErrorScreen from "@screens/Error";
+import { Provider } from "react-redux";
+import store from "store";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +21,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />;
+    </Provider>
+  );
 };
 
 export default App;
---
+++ client/src/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "/api/v1";
+export const USERS_URL = "/users";
+export const PORTFOLIO_URL = "/portfolio";
